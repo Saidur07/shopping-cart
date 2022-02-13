b9667d87fb624bfdaa85960a00418753b3322319
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-02-13 12:24:39 +0600

    Scripts added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateCaseNumber(product, price, isIncreasing) {
   let productInput = document.getElementById(product + "-number");
-  let productNumber = productInput.value;
+  let productNumber = parseInt(productInput.value);
   if (isIncreasing) {
     productNumber = parseInt(productNumber) + 1;
   } else if (productNumber > 0) {
@@ -10,6 +10,24 @@ function updateCaseNumber(product, price, isIncreasing) {
   // Update product Total
   let productTotal = document.getElementById(product + "-total");
   productTotal.innerText = productNumber * price;
+
+  calculateTotal();
+}
+
+function getInputValue(product) {
+  let productInput = document.getElementById(product + "-number");
+  let productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+function calculateTotal() {
+  let phoneTotal = getInputValue("phone") * 1219;
+  let caseTotal = getInputValue("case") * 59;
+  let subTotal = phoneTotal + caseTotal;
+  let taxAmount = subTotal / 10;
+  let totalPrice = subTotal + taxAmount;
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("tax-amount").innerText = taxAmount;
+  document.getElementById("total-price").innerText = totalPrice;
 }
 
 document.getElementById("phone-plus").addEventListener("click", function () {
